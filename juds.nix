25c6f3cc7aec3823e44f77156fde784710770a4f
Herwig Hochleitner <herwig@bendlas.net>
2017-01-10 12:03:52 +0100

    fix juds builder

--- juds.nix
+++ juds.nix
@@ -1,25 +1,37 @@
-{ stdenv, jdk, fetchgit, autoconf }:
+{ stdenv, stdenv_32bit, jdk, fetchFromGitHub, fetchurl, autoconf }:
 let
 
-  version = "0.94";
+  version = "0.95";
 
-in stdenv.mkDerivation rec {
-  name = "juds-${version}.jar";
-  src = fetchgit {
-    url = "https://github.com/mcfunley/juds.git";
-    rev = "3545035fc63aeccf3444b447e7959c7675381027";
-    sha256 = "6072dc200f2820544830ddfafeee42beeb4f64697645de3e921fa5ad6684ddb9";
+in stdenv.mkDerivation {
+  name = "juds-${version}";
+  src = fetchFromGitHub {
+    owner = "mcfunley";
+    repo = "juds";
+    rev = "3334ede781240fd52cbc06fdc84243a73c88ea95";
+    sha256 = "0kizlvdax6jwnzvr6y4zfvgqxp6b02isr1bcijnmqhzs6kcdkvjg";
   };
-  nativeBuildInputs = [ jdk autoconf ];
-  phases = [ "unpackPhase" "configurePhase" "buildPhase" "installPhase" ];
-  preConfigure = "sh autoconf.sh";
-  buildPhase = ''
-    make SHELL=${stdenv.shell} \
-         M32="-m64" \
-         PREFIX=$out \
-         nativelib jar
-  '';
-  installPhase = ''
-    cp ${name} $out
+  nativeBuildInputs = [ jdk ];
+  buildInputs = [ <nixpkgs/pkgs/build-support/setup-hooks/separate-debug-info.sh> ];
+  outputs = [ "out" "debug" ];
+  patches = [ ./0001-prepare-for-hardcoded-sopath.patch ];
+
+  buildCommand = ''
+    sopath="$out/lib/libunixdomainsocket.so"
+
+    unpackPhase
+    cd $sourceRoot
+    patchPhase
+    substituteInPlace com/etsy/net/UnixDomainSocket.java \
+      --subst-var sopath
+
+    mkdir -p $out/lib $out/src
+    cp -R com $out/src/com
+
+    javac -sourcepath $out/src -d $out/lib com/etsy/net/{JUDS,UnixDomainSocket,UnixDomainSocketClient,UnixDomainSocketServer}.java
+    javah -sourcepath . -o $out/src/com/etsy/net/UnixDomainSocket.h com.etsy.net.UnixDomainSocket
+    gcc -fPIC -O2 -shared -o $sopath $out/src/com/etsy/net/UnixDomainSocket.c
+
+    fixupPhase
   '';
 }
