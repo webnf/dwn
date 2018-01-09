deadec55162597a905e40689b15270c80a3b1d9d
Herwig Hochleitner <herwig@bendlas.net>
2018-01-09 22:44:29 +0100

    deps.expander: unify versions only up to artifact name
    extension and classifier are ignored in version unification

--- deps.expander/src/webnf/dwn/deps/expander.clj
+++ deps.expander/src/webnf/dwn/deps/expander.clj
@@ -7,6 +7,9 @@
             [clojure.edn :as edn]
             [clojure.set :as set]))
 
+(defn warn [fmt & args]
+  (.println *err* (str "WARNING: " (apply format fmt args))))
+
 (def gvs (GenericVersionScheme.))
 
 (defn- compare-versions [v1 v2]
@@ -26,19 +29,28 @@
                          exclusions scope]
                   :as coordinate}]
             (let [{rv :version
-                   re :exclusions} (get-in r [group artifact extension classifier])
-                  version* (or (get-in fixed-coordinates [group artifact extension classifier])
-                               (and rv (neg? (compare-versions version rv))
-                                    rv)
-                               version)]
-              (let [exclusions* (set/intersection (set re) exclusions)]
-                (if (and (= rv version*)
+                   re :exclusions
+                   rex :extension
+                   rcl :classifier
+                   :as have-result} (get-in r [group artifact])
+                  [extension* classifier* version*] (or (get-in fixed-coordinates [group artifact])
+                                                        [extension classifier
+                                                         (or (and rv (neg? (compare-versions version rv))
+                                                                  rv)
+                                                             version)])]
+              (let [exclusions* (if have-result
+                                  (set/intersection re exclusions)
+                                  exclusions)]
+                (if (and (not have-result)
+                         (= rv version*)
                          (= re exclusions*))
                   r (let [tree-exclusions* (set/union tree-exclusions exclusions*)]
-                      (unify-versions (assoc-in r [group artifact extension classifier]
+                      (unify-versions (assoc-in r [group artifact]
                                                 {:version version*
-                                                 :exclusions exclusions*})
-                                      (->> [group artifact extension classifier version* :dependencies]
+                                                 :exclusions exclusions*
+                                                 :extension extension*
+                                                 :classifier classifier*})
+                                      (->> [group artifact extension* classifier* version* :dependencies]
                                            (get-in repo)
                                            (map coordinate-info)
                                            (remove (exclusions-pred tree-exclusions*)))
@@ -48,36 +60,44 @@
 
 (defn- seen-pred [seen]
   (fn [{:keys [group artifact extension classifier]}]
-    (contains? seen [group artifact extension classifier])))
+    (contains? seen [group artifact])))
 
-(defn- expand-deps* [coordinates seen version-map repo]
-  (mapcat (fn [{:keys [group artifact extension classifier]}]
-            (let [{:keys [version exclusions] :as vmi} (get-in version-map [group artifact extension classifier])]
+(defn- expand-deps* [coordinates seen version-map repo tree-exclusions]
+  (mapcat (fn [{:keys [group artifact] :as coord}]
+            #_(when-not ((exclusions-pred tree-exclusions) coord))
+            (let [{:keys [version exclusions extension classifier] :as vmi}
+                  (get-in version-map [group artifact])
+                  tree-exclusions* (set/union tree-exclusions exclusions)]
               (concat
-               (when-not (contains? seen [group artifact extension classifier])
+               (when-not (contains? seen [group artifact])
                  [[group artifact extension classifier version]])
                (expand-deps* (->> [group artifact extension classifier version]
                                   (get-in repo)
                                   :dependencies
                                   (map coordinate-info)
-                                  (remove (seen-pred seen))
-                                  (remove (exclusions-pred exclusions)))
-                             (conj seen [group artifact extension classifier])
-                             version-map repo))))
+                                  #_(remove (seen-pred seen))
+                                  (remove (exclusions-pred tree-exclusions*)))
+                             (conj seen [group artifact])
+                             version-map repo
+                             tree-exclusions*))))
           coordinates))
 
 (defn- provided->seen [provided]
-  (transduce (map (comp vec (partial take 4)))
-             conj #{} provided))
+  (into #{}
+        (comp (map coordinate-info)
+              (map (juxt :group :artifact))
+              (map vec))
+        provided))
+
 (defn expand-deps [coordinates' fixed-coordinates' provided-versions repo]
   (let [coordinates (map coordinate-info coordinates')
         fixed-coordinates (reduce (fn [c fc]
                                     (let [{:keys [group artifact extension classifier version]}
                                           (coordinate-info fc)]
-                                      (assoc-in c [group artifact extension classifier] version)))
+                                      (assoc-in c [group artifact] [extension classifier version])))
                                   {} fixed-coordinates')
         version-map (unify-versions {} coordinates fixed-coordinates repo #{})]
-    (->> (expand-deps* coordinates (provided->seen provided-versions) version-map repo)
+    (->> (expand-deps* coordinates (provided->seen provided-versions) version-map repo #{})
          reverse distinct reverse
          (mapv (fn [coord]
                  (-> (get-in repo coord)
@@ -87,9 +107,6 @@
 (defn read* [f]
   (with-open [i (PushbackReader. (io/reader f))]
     (edn/read i)))
-
-(defn warn [fmt & args]
-  (.println *err* (str "WARNING: " (apply format fmt args))))
 
 (defn tree-level-kxf [kxf]
   (fn
@@ -119,13 +136,13 @@
      ([r] r)
      ([r [version classifier extension artifact group :as p]
        {:strs [sha1 dirs jar dependencies] :as desc}]
-      (warn "Overriding [%s/%s \"%s\"]:\n  %s" group artifact version (pr-str desc))
+      ;; (warn "Overriding [%s/%s \"%s\"]:\n  %s" group artifact version (pr-str desc))
       (assoc-in r [group artifact extension classifier version]
                 {:sha1 sha1 :dirs dirs :jar jar :dependencies dependencies})))
    repo overlay))
 
 (defn -main [classpath-out-file repo-file coordinates-str fixed-coordinates-str provided-versions-str overlay-repo-str]
-  (warn "EXPANDER: %s %s \n %s" classpath-out-file repo-file overlay-repo-str)
+  ;; (warn "EXPANDER: %s %s \n %s" classpath-out-file repo-file overlay-repo-str)
   (let [classpath (expand-deps (edn/read-string coordinates-str)
                                (edn/read-string fixed-coordinates-str)
                                (edn/read-string provided-versions-str)
